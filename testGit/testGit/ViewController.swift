9bc195290ecde8b69febdc729eca8e484885de90
liujunjie <729403101@qq.com>
2019-07-20 15:04:34 +0800

    init Project

---
+++ testGit/testGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by LJJ on 2019/7/20.
+//  Copyright © 2019 junjie.liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
